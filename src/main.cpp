3dea9ca360e5337bdc143cc5ee0f9a0395c057f9
kesslern <nathant93@gmail.com>
2015-07-11 23:18:13 -0400

    Basic FPS counter on stdout

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,24 @@ int main()
   srand(time(NULL));
   Game game(800, 600);
 
-  while (game.run()) {}
+  int frames = 0;
+  Uint32 startTime = SDL_GetTicks();
+  
+  while (game.run())
+    {
+      Uint32 time = SDL_GetTicks();
+
+      /* Update FPS every 3 seconds. */
+      if (time - startTime > 3000)
+	{
+	  int fps = frames / 3;
+	  printf("fps: %d\n", fps);
+	  frames = 0;
+	  startTime = time;
+	}
+      
+      frames++;
+    }
   
   return 0;
 }
